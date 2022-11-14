01e143f3516f9f5fd77961152a35a9fad4e9c3c5
Cory W. Mauer <corymauer@gmail.com>
2022-11-13 20:34:26 -0700

    adding aws amplify backend

---
+++ src/server/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "messagefunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "messageapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
